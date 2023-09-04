ffb5e44fd4f7c36b3ef5709c24ad22844dc33b89
GTSilver <gtsilver.work@gmail.com>
2023-09-04 17:39:13 +0500

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Image_comparison
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Path1_TB = new System.Windows.Forms.TextBox();
+            this.Path2_TB = new System.Windows.Forms.TextBox();
+            this.Browse1_B = new System.Windows.Forms.Button();
+            this.Browse2_B = new System.Windows.Forms.Button();
+            this.Result_DGV = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Start_B = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Stop_B = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Result_DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Path1_TB
+            // 
+            this.Path1_TB.Location = new System.Drawing.Point(12, 41);
+            this.Path1_TB.Name = "Path1_TB";
+            this.Path1_TB.Size = new System.Drawing.Size(156, 20);
+            this.Path1_TB.TabIndex = 0;
+            // 
+            // Path2_TB
+            // 
+            this.Path2_TB.Location = new System.Drawing.Point(12, 67);
+            this.Path2_TB.Name = "Path2_TB";
+            this.Path2_TB.Size = new System.Drawing.Size(156, 20);
+            this.Path2_TB.TabIndex = 1;
+            // 
+            // Browse1_B
+            // 
+            this.Browse1_B.Location = new System.Drawing.Point(174, 39);
+            this.Browse1_B.Name = "Browse1_B";
+            this.Browse1_B.Size = new System.Drawing.Size(81, 23);
+            this.Browse1_B.TabIndex = 2;
+            this.Browse1_B.Text = "Open";
+            this.Browse1_B.UseVisualStyleBackColor = true;
+            this.Browse1_B.Click += new System.EventHandler(this.Browse1_B_Click);
+            // 
+            // Browse2_B
+            // 
+            this.Browse2_B.Location = new System.Drawing.Point(174, 65);
+            this.Browse2_B.Name = "Browse2_B";
+            this.Browse2_B.Size = new System.Drawing.Size(81, 23);
+            this.Browse2_B.TabIndex = 3;
+            this.Browse2_B.Text = "Open";
+            this.Browse2_B.UseVisualStyleBackColor = true;
+            this.Browse2_B.Click += new System.EventHandler(this.Browse2_B_Click);
+            // 
+            // Result_DGV
+            // 
+            this.Result_DGV.AllowUserToAddRows = false;
+            this.Result_DGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Result_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Result_DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.Result_DGV.Location = new System.Drawing.Point(12, 107);
+            this.Result_DGV.Name = "Result_DGV";
+            this.Result_DGV.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Result_DGV.RowHeadersVisible = false;
+            this.Result_DGV.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Result_DGV.ShowCellErrors = false;
+            this.Result_DGV.ShowCellToolTips = false;
+            this.Result_DGV.ShowEditingIcon = false;
+            this.Result_DGV.ShowRowErrors = false;
+            this.Result_DGV.Size = new System.Drawing.Size(243, 154);
+            this.Result_DGV.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Collor";
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 48;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Number";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 64;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Percent, %";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column3.Width = 64;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "SD";
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column4.Width = 64;
+            // 
+            // Start_B
+            // 
+            this.Start_B.Location = new System.Drawing.Point(174, 12);
+            this.Start_B.Name = "Start_B";
+            this.Start_B.Size = new System.Drawing.Size(81, 23);
+            this.Start_B.TabIndex = 5;
+            this.Start_B.Text = "Start";
+            this.Start_B.UseVisualStyleBackColor = true;
+            this.Start_B.Click += new System.EventHandler(this.Start_B_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Paths to Images for Comparison";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 93);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(243, 8);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Stop_B
+            // 
+            this.Stop_B.Location = new System.Drawing.Point(174, 12);
+            this.Stop_B.Name = "Stop_B";
+            this.Stop_B.Size = new System.Drawing.Size(81, 23);
+            this.Stop_B.TabIndex = 8;
+            this.Stop_B.Text = "Stop";
+            this.Stop_B.UseVisualStyleBackColor = true;
+            this.Stop_B.Click += new System.EventHandler(this.Stop_B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 273);
+            this.Controls.Add(this.Stop_B);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start_B);
+            this.Controls.Add(this.Result_DGV);
+            this.Controls.Add(this.Browse2_B);
+            this.Controls.Add(this.Browse1_B);
+            this.Controls.Add(this.Path2_TB);
+            this.Controls.Add(this.Path1_TB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Image Comparer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Result_DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Path1_TB;
+        private System.Windows.Forms.TextBox Path2_TB;
+        private System.Windows.Forms.Button Browse1_B;
+        private System.Windows.Forms.Button Browse2_B;
+        private System.Windows.Forms.DataGridView Result_DGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button Start_B;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Stop_B;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_comparison
+{
+    public partial class Form1 : Form
+    {
+        static int progressValue;
+        static int progressMaxValue;
+        static Thread progressThread;
+        static Thread calculationThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class ImagePixel
+        {
+            public float R;
+            public float G;
+            public float B;
+            public float A;
+
+            public float GetSum()
+            {
+                return R + G + B + A;
+            }
+
+            public float GetAverage()
+            {
+                return (R + G + B + A) / 4;
+            }
+
+            public bool IsDifferent()
+            {
+                if (R != 0 || G != 0 || B != 0 || A != 0)
+                    return true;
+                else
+                    return false;
+            }
+
+            public static ImagePixel operator -(ImagePixel pixel1, ImagePixel pixel2)
+            {
+                return new ImagePixel
+                {
+                    R = pixel1.R - pixel2.R,
+                    G = pixel1.G - pixel2.G,
+                    B = pixel1.B - pixel2.B,
+                    A = pixel1.A - pixel2.A,
+                };
+            }
+
+            public static ImagePixel operator *(ImagePixel pixel1, ImagePixel pixel2)
+            {
+                return new ImagePixel
+                {
+                    R = pixel1.R * pixel2.R,
+                    G = pixel1.G * pixel2.G,
+                    B = pixel1.B * pixel2.B,
+                    A = pixel1.A * pixel2.A,
+                };
+            }
+        }
+
+        public class Сhannel
+        {
+            public string Name;
+            public int Number;
+            public float Percente;
+            public float Sum;
+            public float SumSquares;
+            public float Dispersion;
+            public float StandardDeviation;
+
+            public void GetPercente(int pixelsNumber, int digits)
+            {
+                Percente = (float)Math.Round(Number * 100f / pixelsNumber, digits);
+            }
+
+            public void GetDispersion(int pixelsNumber, int digits)
+            {
+                Dispersion = (float)Math.Round((SumSquares / pixelsNumber) - (Sum / pixelsNumber) * (Sum / pixelsNumber), digits);
+            }
+
+            public void GetStandardDeviation(int digits)
+            {
+                StandardDeviation = (float)Math.Round(Math.Sqrt(Dispersion), digits);
+            }
+
+            public void CalculateParameters(int pixelsNumber, int digits)
+            {
+                GetPercente(pixelsNumber, digits);
+                GetDispersion(pixelsNumber, digits);
+                GetStandardDeviation(digits);
+            }
+        }
+
+        public ImagePixel[,] GetImage(Bitmap bmp)    //Использование object выполнено для передачи объекта Bitmap в отдельный поток
+        {
+            //Кастует Bitmap обратно в Bitmap из object для дальнейшей работы
+            Bitmap bmap = (Bitmap)bmp;
+
+            //Создаём массив для хранения и передачи изображения (массива пикселей) для заполнения в следующем цикле
+            var result = new ImagePixel[bmap.Width, bmap.Height];
+
+            //Требуется для использования указателей * (необходимо включить в свойствах сборки "Небезопасный код")
+            unsafe
+            {
+                //Фиксирует каждый пиксель прямоугольной области, равной размеру изображения, на режим чтения и записи соответствующего формата пикселей (Format24bppRgb или Format32bppArgb), в памяти
+                BitmapData bitmapData = bmap.LockBits(new Rectangle(0, 0, bmap.Width, bmap.Height), ImageLockMode.ReadWrite, bmap.PixelFormat);
+
+                //Определяем формат пикселей
+                var pixelFormat = bmap.PixelFormat;
+
+                //Определяем сколько байт тратиться на один пиксель
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bmap.PixelFormat) / 8;
+
+                //Определяем высоту изображения в пикселях
+                int heightInPixels = bitmapData.Height;
+
+                //Определяем ширину изображения в байтах
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+                //Создаём указатель указывающий на первый пиксель изображения (на первый байт первого пикселя, в фиксированной области данных)
+                byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+                
+                //Выполняем цикл аналогичный for (var y = 0; y < heightInPixels; y++) c автоматическим распаралеливанием каждой итерации
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    progressValue++;
+                    //Создаём указатель указывающий на первый байт y-ой строки (.Stride - возвращает ширину строки в байтах)
+                    byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
+
+                    /*Выполняем цикл шагающий по каждому пикселю (каждому байту каждого пикселя) y-ой строки
+                    При использовании указателя типа byte порядок сохранения цветов будет BGR для Format24bppRgb и BGRA для Format32bppArgb соответственно.
+                    При касте указателя к int порядок байт будет соответствовать RGB и ARGB соответственно
+                    (https://stackoverflow.com/questions/8104461/pixelformat-format32bppargb-seems-to-have-wrong-byte-order).
+                    Для доступа к каждому цвету, при byte указателе, используется следующий код: currentLine[x] - Blue; currentLine[x + 1] - Green; и т.д.*/
+                    if (pixelFormat == PixelFormat.Format24bppRgb || pixelFormat == PixelFormat.Format32bppRgb)
+                        for (var x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                        {
+                            var xIndex = x / bytesPerPixel;
+                            result[xIndex, y] = new ImagePixel();
+
+                            result[xIndex, y].B = currentLine[x];
+                            result[xIndex, y].G = currentLine[x + 1];
+                            result[xIndex, y].R = currentLine[x + 2];
+                            result[xIndex, y].A = 255;
+                        }
+                    else if (pixelFormat == PixelFormat.Format32bppArgb)
+                        for (var x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                        {
+                            var xIndex = x / bytesPerPixel;
+                            result[xIndex, y] = new ImagePixel();
+
+                            result[xIndex, y].B = currentLine[x];
+                            result[xIndex, y].G = currentLine[x + 1];
+                            result[xIndex, y].R = currentLine[x + 2];
+                            result[xIndex, y].A = currentLine[x + 3];
+                        }
+                    else
+                        MessageBox.Show("Error:\tIncorrect pixel format.\tOnly the following formats are available:\tRGB 24 bit per pixel;\tRGB 32 bit per pixel;\t ARGB 32 bit per pixel.");
+                });
+
+                //Разблокирует указанную область памяти Bitmap
+                bmap.UnlockBits(bitmapData);
+            }
+            return result;
+        }
+
+        public void UIOff()
+        {
+            Invoke((MethodInvoker)delegate ()
+            {
+                Start_B.Enabled = false;
+                Start_B.Visible = false;
+                Stop_B.Enabled = true;
+                Stop_B.Visible = true;
+                Browse1_B.Enabled = false;
+                Browse2_B.Enabled = false;
+                Path1_TB.Enabled = false;
+                Path2_TB.Enabled = false;
+                progressBar1.Value = 0;
+            });
+        }
+
+        public void UIOn()
+        {
+            Invoke((MethodInvoker)delegate ()
+            {
+                Start_B.Enabled = true;
+                Start_B.Visible = true;
+                Stop_B.Enabled = false;
+                Stop_B.Visible = false;
+                Browse1_B.Enabled = true;
+                Browse2_B.Enabled = true;
+                Path1_TB.Enabled = true;
+                Path2_TB.Enabled = true;
+                progressBar1.Value = 0;
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UIOn();
+            Result_DGV.Rows.Add("  R");
+            Result_DGV.Rows.Add("  G");
+            Result_DGV.Rows.Add("  B");
+            Result_DGV.Rows.Add("  A");
+            Result_DGV.Rows.Add("  Σ");
+            Result_DGV.Rows.Add("<Σ>");
+        }
+
+        private void Browse1_B_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "images| *.jpg; *.png; *.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    Path1_TB.Text = ofd.FileName;
+            }
+        }
+
+        private void Browse2_B_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "images| *.jpg; *.png; *.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    Path2_TB.Text = ofd.FileName;
+            }
+        }
+
+        private void CalculationProcedure()
+        {
+            var i1 = Image.FromFile(Path1_TB.Text);
+            var i2 = Image.FromFile(Path2_TB.Text);
+            var inputImage1 = new Bitmap(i1);
+            var inputImage2 = new Bitmap(i2);
+
+            int imageWidth = inputImage1.Width;
+            int imageHeight = inputImage1.Height;
+            int imagePixelsNumber = imageWidth * imageHeight;
+
+            if (inputImage1.Height == inputImage2.Height && inputImage1.Width == inputImage2.Width)
+            {
+                progressValue = 0;
+                progressMaxValue = imageHeight * 3;
+
+                Invoke((MethodInvoker)delegate ()
+                {
+                    progressBar1.Maximum = progressMaxValue;
+                });
+
+                var image1 = GetImage(inputImage1);
+
+                progressValue = inputImage1.Height;
+
+                var image2 = GetImage(inputImage2);
+
+                progressValue = inputImage2.Height * 2;
+
+                var red = new Сhannel() { Name = "Red"};
+                var green = new Сhannel() { Name = "Green" };
+                var blue = new Сhannel() { Name = "Blue" };
+                var alpha = new Сhannel() { Name = "Alpha" };
+                var sum = new Сhannel() { Name = "Sum" };
+                var average = new Сhannel() { Name = "Average" };
+
+                for (var y = 0; y < imageHeight; y++)
+                {
+                    progressValue++;
+                    for (var x = 0; x < imageWidth; x++)
+                    {
+                        var pixel = image1[x, y] - image2[x, y];
+                        float sumDelta = pixel.GetSum();
+                        float averageDelta = pixel.GetAverage();
+
+                        if (pixel.R != 0) red.Number++;
+                        if (pixel.G != 0) green.Number++;
+                        if (pixel.B != 0) blue.Number++;
+                        if (pixel.A != 0) alpha.Number++;
+                        if (pixel.IsDifferent()) sum.Number++;
+
+                        red.Sum += pixel.R;
+                        green.Sum += pixel.G;
+                        blue.Sum += pixel.B;
+                        alpha.Sum += pixel.A;
+                        sum.Sum += sumDelta;
+                        average.Sum += averageDelta;
+
+                        var pixelSquares = pixel * pixel;
+
+                        red.SumSquares += pixelSquares.R;
+                        green.SumSquares += pixelSquares.G;
+                        blue.SumSquares += pixelSquares.B;
+                        alpha.SumSquares += pixelSquares.A;
+                        sum.SumSquares += sumDelta * sumDelta;
+                        average.SumSquares += averageDelta * averageDelta;
+                    }
+                }
+                average.Number = sum.Number / 4;
+
+                var channelList = new List<Сhannel>();
+
+                channelList.Add(red);
+                channelList.Add(green);
+                channelList.Add(blue);
+                channelList.Add(alpha);
+                channelList.Add(sum);
+                channelList.Add(average);
+
+                foreach (var unit in channelList)
+                    unit.CalculateParameters(imagePixelsNumber, 4);
+
+                try { progressThread.Abort(); }
+                catch { }
+
+                Invoke((MethodInvoker)delegate ()
+                {
+                    Result_DGV.Rows.Clear();
+                    progressBar1.Value = 0;
+                    foreach (var unit in channelList)
+                        Result_DGV.Rows.Add(unit.Name, unit.Number, unit.Percente, unit.StandardDeviation);
+
+                    inputImage1.Dispose();
+                    inputImage2.Dispose();
+                    i1.Dispose();
+                    i2.Dispose();
+
+                    channelList = null;
+                    inputImage1 = null;
+                    inputImage2 = null;
+                    image1 = null;
+                    image2 = null;
+                    red = null;
+                    green = null;
+                    blue = null;
+                    alpha = null;
+                    sum = null;
+                    average = null;
+
+                    imageWidth = 0;
+                    imageHeight = 0;
+                    imagePixelsNumber = 0;
+                    progressValue = 0;
+                    progressMaxValue = 0;
+
+                    GC.Collect();
+
+                    UIOn();
+                });
+
+                try { calculationThread.Abort(); }
+                catch { }
+            }
+            else
+                MessageBox.Show("Error:\tImages differ in height and/or width.\tFor comparison, images of equal dimensions are required.");
+        }
+
+        private void ProgressProcedure()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                Invoke((MethodInvoker)delegate ()
+                {
+                    progressBar1.Value = progressValue;
+                });
+            }
+        }
+
+        private void Start_B_Click(object sender, EventArgs e)
+        {
+            UIOff();
+
+            calculationThread = new Thread(new ThreadStart(CalculationProcedure));
+            progressThread = new Thread(new ThreadStart(ProgressProcedure));
+
+            calculationThread.Start();
+            progressThread.Start();
+        }
+
+        private void Stop_B_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculationThread.Abort();
+                progressThread.Abort();
+            }
+            catch { }
+
+            UIOn();
+        }
+    }
+}
